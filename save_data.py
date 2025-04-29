77c5224a2906453966dd0d0083a478e63a39108e
Olivier Friard <olivier.friard@unito.it>
2025-04-29 12:15:25 +0200

    some changes

--- save_data.py
+++ save_data.py
@@ -4,7 +4,7 @@ Save data
 
 def save_data_json(viewer):
     d = {
-        "angle_deg":  viewer.angle_deg
+        "angle_deg":  viewer.angle_deg,
         "image_file_name":  viewer.nome_file
            }
     print(f"{d=}")
